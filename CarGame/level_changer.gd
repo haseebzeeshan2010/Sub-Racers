b37f969868395be4f0b2f2524e7743f35b399906
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2026-03-12 16:48:00 +0000

    Fix Minor Issues with Level Changer and Add Export Presets

--- CarGame/level_changer.gd
+++ CarGame/level_changer.gd
@@ -1,52 +1,48 @@
 extends Control
 
 var scene_to_change = ""
-# Called when the node enters the scene tree for the first time.
+
+var tutorial_scene = preload("res://CarGame/Tutorial.tscn")
+var level1_scene = preload("res://CarGame/Level1.tscn")
+var level2_scene = preload("res://CarGame/Level2.tscn")
+var start_scene = preload("res://CarGame/StartScene.tscn")
+
+var scenes = {
+	"Tutorial": tutorial_scene,
+	"Level1": level1_scene,
+	"Level2": level2_scene,
+	"StartScene": start_scene
+}
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
 
+func _process(delta: float) -> void:
+	pass
 
 func _on_start_tutorial_button_pressed() -> void:
 	print("start tutorial")
 	%TransitionPlayer.play("StartSceneExit_LevelChanger")
 	scene_to_change = "Tutorial"
-	pass # Replace with function body.
-
 
 func _on_start_level_button_pressed() -> void:
 	print("start level 1: Murky Waters")
 	%TransitionPlayer.play("StartSceneExit_LevelChanger")
 	scene_to_change = "Level1"
-	pass # Replace with function body.
-
 
 func _on_start_level2_button_pressed() -> void:
 	print("start level 2: Thermal Trench")
 	%TransitionPlayer.play("StartSceneExit_LevelChanger")
 	scene_to_change = "Level2"
-	pass # Replace with function body.
-
 
 func _on_title_screen_button_pressed() -> void:
 	scene_to_change = "StartScene"
 	%TransitionPlayer.play("StartSceneExit_LevelChanger_2")
 	print("start title screen")
-	pass # Replace with function body.
-
 
 func _on_transition_player_animation_finished(anim_name: StringName) -> void:
-	get_tree().change_scene_to_file("res://CarGame/"+scene_to_change+".tscn")
-	pass # Replace with function body.
-
+	if scene_to_change in scenes:
+		get_tree().change_scene_to_packed(scenes[scene_to_change])
 
 func _on_transition_player_animation_started(anim_name: StringName) -> void:
-	#if anim_name == "StartSceneEnter":
-		#get_node("/root/GlobalAudio").selection_music()
-	#if anim_name == "StartSceneExit_LevelChanger":
-		#get_node("/root/GlobalAudio").levelmusic()
-	pass # Replace with function body.
+	pass
